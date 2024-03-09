17698d1e562ff105a7821981770dbca9aab76f34
akm <akm2000@gmail.com>
2024-03-09 23:52:17 +0900

    :sparkles: 対象の拡張子が .go ならば go fmt を呼び出し

--- lib/fukowl.rb
+++ lib/fukowl.rb
@@ -10,6 +10,12 @@ module Fukowl
       content = File.read(target_path)
       yield(content)
       open(target_path, 'w'){|f| f.puts(content) }
+
+      case File.extname(target_path)
+      when '.go'
+        system("go fmt #{target_path}", exception: true)
+      end
+
     end
   end
 end
